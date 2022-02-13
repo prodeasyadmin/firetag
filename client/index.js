431ba12f30c2e1824589cfa3e74788ded42a03eb
firetag <amritag2004@gmail.com>
2022-02-13 15:44:54 +0530

    initial

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
-var http = require('http');
+const http = require('http');
 var fs = require('fs');
-http.createServer(function(req, res){
+const server = http.createServer(function(req, res){
     fs.readFile('index.html', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-        return res.end();
+        res.end();
     });
-}).listen(8080);
+})
+
+server.listen(8080);
--- client/main.js
+++ client/main.js
@@ -1,9 +1,37 @@
 
+
+
+
+
+
+
+import { Client } from "@googlemaps/google-maps-services-js";
+
+const client = new Client({});
+
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 45, lng: -110 }],
+      key: "asdf",
+    },
+    timeout: 1000, // milliseconds
+  })
+  .then((r) => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch((e) => {
+    console.log(e.response.data.error_message);
+  });
+
+
+
 let map, infoWindow;
 
 
 
-// searchbox
+
+  // searchbox
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -33.8688, lng: 151.2195 },
