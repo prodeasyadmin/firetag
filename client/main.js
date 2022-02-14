fd60e487265557515085271c75bc05e20601377f
firetag <amritag2004@gmail.com>
2022-02-14 13:43:31 +0530

    api_library

--- client/main.js
+++ client/main.js
@@ -1,29 +1,5 @@
 
-
-
-
-
-
-
-import { Client } from "@googlemaps/google-maps-services-js";
-
-const client = new Client({});
-
-client
-  .elevation({
-    params: {
-      locations: [{ lat: 45, lng: -110 }],
-      key: "asdf",
-    },
-    timeout: 1000, // milliseconds
-  })
-  .then((r) => {
-    console.log(r.data.results[0].elevation);
-  })
-  .catch((e) => {
-    console.log(e.response.data.error_message);
-  });
-
+const {Client} = require("@googlemaps/google-maps-services-js");
 
 
 let map, infoWindow;
