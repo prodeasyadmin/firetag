db462f65d6c8ede8a60591fd211dadc423547b26
firetag <amritag2004@gmail.com>
2022-04-27 15:47:25 +0530

    working button

---
+++ admin/admin.js
@@ -0,0 +1,8 @@
+// var coordinates, url;
+
+// $("#map").on('click', function () {
+//   coordinates = $('#coordinates').val();
+//   url         = "https://www.google.com.sa/maps/search/"+ coordinates +",12.21z?hl=en";
+
+//   window.open(url, '_blank');
+// });
